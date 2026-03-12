513ea19f74769dc43345b6b3b45bb12908a1b8ae
luispf1290 <luispia1290@gmail.com>
2026-03-11 21:34:15 -0600

    feature: se modifica AuthController

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/controllers/AuthController.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/controllers/AuthController.java
@@ -1,41 +1,25 @@
 package com.nohadev.puntoventa.core.security.controllers;
 
-import com.nohadev.puntoventa.core.security.service.JWTService;
-import com.nohadev.puntoventa.core.security.service.UsuarioDetailServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.nohadev.puntoventa.core.security.dto.AuthenticationResponse;
+import com.nohadev.puntoventa.core.security.dto.AuthenticationRequest;
+import com.nohadev.puntoventa.core.security.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UsuarioDetailServiceImpl usuarioDetailService;
-    @Autowired
-    private JWTService jwtService;
+    private final AuthService authService;
 
-    public ResponseEntity<AuthenticationResponse> createToken(@RequestBody AuthenticationRquest request){
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken()
-            );
-        }catch (){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        final UserDetails userDetails = usuarioDetailService.loadUserByUsername();
-        final String jwtToken = jwtService.generateToken(userDetails);
-
-        return ResponseEntity.ok(new AuthenticationResponse(jwtToken));
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authService.Login(request));
     }
 
 }
