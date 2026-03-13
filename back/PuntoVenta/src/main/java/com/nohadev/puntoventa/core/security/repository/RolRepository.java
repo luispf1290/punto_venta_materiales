64ef2d851783b1fb59d1b3cc6c44b7dff3df9f09
luispf1290 <luispia1290@gmail.com>
2026-03-12 20:33:46 -0600

    feature: se agrega RolRepository

---
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/repository/RolRepository.java
@@ -0,0 +1,12 @@
+package com.nohadev.puntoventa.core.security.repository;
+
+import com.nohadev.puntoventa.core.security.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RolRepository  extends JpaRepository<Rol, Long> {
+     Optional<Rol> findByNombre(String nombre);
+}
