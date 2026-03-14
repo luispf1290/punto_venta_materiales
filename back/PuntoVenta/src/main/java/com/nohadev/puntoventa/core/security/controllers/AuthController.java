8ed8170a2353753b35cd66b207be1e81b8fcecb1
luispf1290 <luispia1290@gmail.com>
2026-03-13 21:56:27 -0600

    feature: se agrega endpoint logout y refreshtoken

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/controllers/AuthController.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/controllers/AuthController.java
@@ -2,7 +2,12 @@ package com.nohadev.puntoventa.core.security.controllers;
 
 import com.nohadev.puntoventa.core.security.dto.AuthenticationResponse;
 import com.nohadev.puntoventa.core.security.dto.AuthenticationRequest;
+import com.nohadev.puntoventa.core.security.dto.RefreshRequest;
+import com.nohadev.puntoventa.core.security.entity.RefreshToken;
+import com.nohadev.puntoventa.core.security.repository.RefreshTokenRepository;
 import com.nohadev.puntoventa.core.security.service.AuthService;
+import com.nohadev.puntoventa.core.security.service.RefreshTokenService;
+import com.nohadev.puntoventa.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +15,57 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtUtil jwtUtil;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authService.Login(request));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@RequestBody RefreshRequest request){
+        RefreshToken token = refreshTokenRepository
+                .findByToken(request.getRefreshToken())
+                .orElseThrow();
+        refreshTokenRepository.delete(token);
+
+        return ResponseEntity.ok("Logged out successfully");
+
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthenticationResponse> refresh(@RequestBody RefreshRequest request){
+
+        RefreshToken refreshToken =
+                refreshTokenRepository
+                        .findByToken(request.getRefreshToken())
+                        .orElseThrow();
+
+        if(refreshToken.getExpiryDate().isBefore(Instant.now())){
+            refreshTokenRepository.delete(refreshToken);
+            throw new RuntimeException("Refresh token expired");
+        }
+
+        String username = refreshToken.getUsuario().getUsername();
+
+        String newAccessToken = jwtUtil.generateToken(username);
+
+        refreshTokenRepository.delete(refreshToken);
+        RefreshToken newRefreshToken = refreshTokenService.createRefreshToken(username);
+
+        return ResponseEntity.ok(
+                new AuthenticationResponse(newAccessToken,
+                newRefreshToken.getToken()));
     }
 
 }
