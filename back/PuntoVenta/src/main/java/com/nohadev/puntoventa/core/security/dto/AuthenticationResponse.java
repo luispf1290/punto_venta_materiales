25b8b36a2262af200fd6f11863fabcf8f3fbee46
luispf1290 <luispia1290@gmail.com>
2026-03-13 21:08:51 -0600

    feature: se se agrega propiedad refreshToken

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/dto/AuthenticationResponse.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/core/security/dto/AuthenticationResponse.java
@@ -1,17 +1,15 @@
 package com.nohadev.puntoventa.core.security.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@Getter
+@Setter
 public class AuthenticationResponse {
     private String jwToken;
-
-    public String getJwToken() {
-        return jwToken;
-    }
-
-    public AuthenticationResponse(String jwToken) {
-        this.jwToken = jwToken;
-    }
-
-    public void setJwToken(String jwToken) {
-        this.jwToken = jwToken;
-    }
+    private String refreshToken;
 }
