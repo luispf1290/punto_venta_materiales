aa8eb1e419d56003a08ed0ce0260b788cac14ffb
luispf1290 <luispia1290@gmail.com>
2026-03-16 17:05:00 -0600

    feature: se agrega DTO ProductoRequest

---
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/catalogo/dto/ProductoRequest.java
@@ -0,0 +1,22 @@
+package com.nohadev.puntoventa.catalogo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProductoRequest {
+   private  String codigo_barras;
+   private  String sku;
+   private String descripcion;
+   private Double costo_promedio;
+   private Double precio_venta;
+   private Double stock_minimo;
+   private Boolean activo;
+   private Long categoriaId;
+   private Long marca_id;
+}
