c2718b50b0b56ee78d99ad26545790aafe4859be
luispf1290 <luispia1290@gmail.com>
2026-03-17 19:19:30 -0600

    feature: se agrega anotaciones y se reestructura codigo

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/inventario/entity/MovimientosInventario.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/inventario/entity/MovimientosInventario.java
@@ -2,7 +2,12 @@ package com.nohadev.puntoventa.inventario.entity;
 
 import com.nohadev.puntoventa.catalogo.entity.Producto;
 import com.nohadev.puntoventa.core.security.entity.Usuario;
+import com.nohadev.puntoventa.shared.Enums.TipoMovimiento;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,130 +17,46 @@ import java.time.LocalDate;
 indexes = {
         @Index(name="idx_kardex_producto_fecha", columnList = "fecha")
 })
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Builder
 public class MovimientosInventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String tipo_movimiento;
+    private TipoMovimiento tipo_movimiento;
+
     private String tipo_documento;
+
     private Long id_documento;
-    @Column(nullable = false)
-    private Double cantidad;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal cantidad;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal existenciaAnterior;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal existenciaNueva;
-    @Column(nullable = false)
-    private Double costo_unitario;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal costo_unitario;
+
     @Column(nullable = false)
     private LocalDate fecha;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "producto_id")
     private Producto producto;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "almacen_id")
     private Almacen almacen;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
-
-    public MovimientosInventario() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTipo_movimiento() {
-        return tipo_movimiento;
-    }
-
-    public void setTipo_movimiento(String tipo_movimiento) {
-        this.tipo_movimiento = tipo_movimiento;
-    }
-
-    /* Tipo de documento : Venta,  Compra, Inventario*/
-    public String getTipo_documento() {
-        return tipo_documento;
-    }
-
-    public void setTipo_documento(String tipo_documento) {
-        this.tipo_documento = tipo_documento;
-    }
-
-    /* id del documento realizado */
-    public Long getId_documento() {
-        return id_documento;
-    }
-
-    public void setId_documento(Long id_documento) {
-        this.id_documento = id_documento;
-    }
-
-    public Double getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(Double cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public Double getCosto_unitario() {
-        return costo_unitario;
-    }
-
-    public void setCosto_unitario(Double costo_unitario) {
-        this.costo_unitario = costo_unitario;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public Producto getProducto() {
-        return producto;
-    }
-
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
-    public Almacen getAlmacen() {
-        return almacen;
-    }
-
-    public void setAlmacen(Almacen almacen) {
-        this.almacen = almacen;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public BigDecimal getExistenciaAnterior() {
-        return existenciaAnterior;
-    }
-
-    public void setExistenciaAnterior(BigDecimal existenciaAnterior) {
-        this.existenciaAnterior = existenciaAnterior;
-    }
-
-    public BigDecimal getExistenciaNueva() {
-        return existenciaNueva;
-    }
-
-    public void setExistenciaNueva(BigDecimal existenciaNueva) {
-        this.existenciaNueva = existenciaNueva;
-    }
 }
