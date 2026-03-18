f0f77e4c2c30feef6aaa35e8c49267bd6e06b7e8
luispf1290 <luispia1290@gmail.com>
2026-03-17 19:14:28 -0600

    feature: se agrega anotaciones y mejorar de la entidad

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/inventario/entity/Inventario.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/inventario/entity/Inventario.java
@@ -2,54 +2,33 @@ package com.nohadev.puntoventa.inventario.entity;
 
 import com.nohadev.puntoventa.catalogo.entity.Producto;
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
 
 @Entity
-@Table(name = "inventario")
+@Table(name = "inventario",
+uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"producto_id", "almacen_id"})
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Inventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private Double exitencia_actual;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal exitencia_actual;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "producto_id")
     private Producto producto;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "almacen_id")
     private Almacen almacen;
-
-    public Inventario() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getExitencia_actual() {
-        return exitencia_actual;
-    }
-
-    public void setExitencia_actual(Double exitencia_actual) {
-        this.exitencia_actual = exitencia_actual;
-    }
-
-    public Producto getProducto() {
-        return producto;
-    }
-
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
-    public Almacen getAlmacen() {
-        return almacen;
-    }
-
-    public void setAlmacen(Almacen almacen) {
-        this.almacen = almacen;
-    }
 }
