656b4fb7c6fab1fb62004a6c1dc627fb5b4693b9
luispf1290 <luispia1290@gmail.com>
2026-03-22 19:54:35 -0600

    refactor: se actualiza campos del request

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/inventario/dto/MovimientoInventarioRequest.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/inventario/dto/MovimientoInventarioRequest.java
@@ -1,9 +1,8 @@
 package com.nohadev.puntoventa.inventario.dto;
 
-import com.nohadev.puntoventa.catalogo.entity.Producto;
-import com.nohadev.puntoventa.core.security.entity.Usuario;
-import com.nohadev.puntoventa.inventario.entity.Almacen;
+
 import com.nohadev.puntoventa.shared.Enums.TipoMovimiento;
+import com.nohadev.puntoventa.shared.Enums.TipoOperacion;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +17,12 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class MovimientoInventarioRequest {
     private TipoMovimiento tipo_movimiento;
+    private TipoOperacion tipo_operacion;
     private String tipo_documento;
     private Long id_documento;
     private BigDecimal cantidad;
-    /*private BigDecimal existenciaAnterior;
-    private BigDecimal existenciaNueva;*/
+    private BigDecimal existenciaAnterior;
+    private BigDecimal existenciaNueva;
     private BigDecimal costo_unitario;
     private LocalDate fecha;
     private Long productoId;
