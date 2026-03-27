5a5ea4bd90184ff61308ed51ad55ffce65c41ae3
luispf1290 <luispia1290@gmail.com>
2026-03-26 21:41:43 -0600

    feature: se crea CompraServiceImpl

---
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/compras/service/CompraServiceImpl.java
@@ -0,0 +1,100 @@
+package com.nohadev.puntoventa.compras.service;
+
+import com.nohadev.puntoventa.catalogo.entity.Producto;
+import com.nohadev.puntoventa.compras.dto.CompraRequest;
+import com.nohadev.puntoventa.compras.dto.CompraResponse;
+import com.nohadev.puntoventa.compras.dto.DetalleCompraDTO;
+import com.nohadev.puntoventa.compras.entity.Compra;
+import com.nohadev.puntoventa.compras.entity.DetalleCompra;
+import com.nohadev.puntoventa.compras.mapper.CompraMapper;
+import com.nohadev.puntoventa.compras.repository.CompraRepository;
+import com.nohadev.puntoventa.compras.repository.DetalleCompraRepository;
+import com.nohadev.puntoventa.inventario.dto.MovimientoInventarioRequest;
+import com.nohadev.puntoventa.inventario.service.MovimientoInventarioService;
+import com.nohadev.puntoventa.shared.Enums.TipoMovimiento;
+import com.nohadev.puntoventa.shared.Enums.TipoOperacion;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CompraServiceImpl implements CompraService {
+
+    private final CompraRepository compraRepository;
+    private final DetalleCompraRepository detalleCompraRepository;
+    private final CompraMapper compraMapper;
+    private final MovimientoInventarioService movimientoInventarioService;
+
+    @Override
+    public CompraResponse crearCompra(CompraRequest compraRequest) {
+
+        //TODO: Implentar generacion el folio de la compra
+        Compra compra = new Compra();
+        compra.setFecha(compraRequest.getFecha());
+        compra.setEstatus("COMPRADA");
+
+        BigDecimal subtotal = BigDecimal.ZERO;
+        BigDecimal importe = BigDecimal.ZERO;
+
+        for (DetalleCompraDTO d : compraRequest.getDetalles()) {
+            importe = d.getCantidad().multiply(d.getCosto_unitario());
+            d.setImporte(importe);
+            subtotal = subtotal.add(d.getImporte());
+        }
+
+        BigDecimal iva = subtotal.multiply(new BigDecimal("0.16"));
+        compra.setSubtotal(subtotal);
+        compra.setIva(iva);
+        compra.setTotal(subtotal.add(iva));
+        compra.setProveedor(compraRequest.getProvedorId());
+
+        Compra compraSave = compraRepository.save(compra);
+
+
+        for(DetalleCompraDTO d: compraRequest.getDetalles()){
+            DetalleCompra detalleCompra = new DetalleCompra();
+            detalleCompra.setCompra(compraSave);
+            detalleCompra.setCantidad(d.getCantidad());
+            detalleCompra.setCosto_unitario(d.getCosto_unitario());
+            detalleCompra.setImporte(d.getImporte());
+            detalleCompra.setProducto(d.getProducto());
+
+            detalleCompraRepository.save(detalleCompra);
+
+            MovimientoInventarioRequest movRequest = new MovimientoInventarioRequest();
+
+            movRequest.setProductoId(d.getProductoId());
+            movRequest.setCantidad(d.getCantidad());
+            movRequest.setTipo_movimiento(TipoMovimiento.ENTRADA);
+            movRequest.setTipo_operacion(TipoOperacion.COMPRA);
+            movRequest.setTipo_documento("Compra ID: " + compraSave.getId());
+            movRequest.setCosto_unitario(d.getCosto_unitario());
+            movRequest.setUsuarioId(d.getUsuarioId());
+            movRequest.setAlmacenId(d.getAlmacenId());
+
+            movimientoInventarioService.procesarMovimientoInventario(movRequest);
+        }
+
+        return compraMapper.toDTO(compraSave);
+    }
+
+    @Override
+    public CompraResponse obtenerCompraPorId(Long id) {
+        Compra compra = compraRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Compra no encontrada"));
+
+        return compraMapper.toDTO(compra);
+    }
+
+    @Override
+    public List<CompraResponse> obtenerTodasLasCompras() {
+        return compraRepository.findAll().stream()
+                .map(compraMapper::toDTO)
+                .toList();
+    }
+}
