9cd9374b141defc511fa288168b823aec9254ded
luispf1290 <luispia1290@gmail.com>
2026-03-26 21:40:04 -0600

    feature: se crea CompraMapper

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/compras/entity/Compra.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/compras/entity/Compra.java
@@ -1,93 +1,35 @@
 package com.nohadev.puntoventa.compras.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 
 @Entity
 @Table(name = "compras")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Compra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private LocalDate fecha;
-    private Double subtotal;
-    private Double iva;
-    private Double total;
+    private BigDecimal subtotal;
+    private BigDecimal iva;
+    private BigDecimal total;
     private String estatus;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "proveedor_id")
     private Provedor proveedor;
 
-    public Compra() {
-    }
-
-    public Compra(Long id, LocalDate fecha, Double subtotal, Double iva, Double total, String estatus, Provedor proveedor) {
-        this.id = id;
-        this.fecha = fecha;
-        this.subtotal = subtotal;
-        this.iva = iva;
-        this.total = total;
-        this.estatus = estatus;
-        this.proveedor = proveedor;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public Double getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(Double subtotal) {
-        this.subtotal = subtotal;
-    }
-
-    public Double getIva() {
-        return iva;
-    }
-
-    public void setIva(Double iva) {
-        this.iva = iva;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    public String getEstatus() {
-        return estatus;
-    }
-
-    public void setEstatus(String estatus) {
-        this.estatus = estatus;
-    }
-
-    public Provedor getProveedor() {
-        return proveedor;
-    }
-
-    public void setProveedor(Provedor proveedor) {
-        this.proveedor = proveedor;
-    }
+    @OneToMany(mappedBy = "compra", cascade = CascadeType.ALL)
+    private List<DetalleCompra> detalles;
 }
