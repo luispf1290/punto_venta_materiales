be27604f1cde55b94b0beece74fb701b34155de1
luispf1290 <luispia1290@gmail.com>
2026-03-28 20:15:33 -0600

    refator: se simplifica codigo

--- back/PuntoVenta/src/main/java/com/nohadev/puntoventa/compras/entity/Provedor.java
+++ back/PuntoVenta/src/main/java/com/nohadev/puntoventa/compras/entity/Provedor.java
@@ -1,11 +1,17 @@
 package com.nohadev.puntoventa.compras.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "proveedores" )
 public class Provedor {
     @Id
@@ -26,81 +32,4 @@ public class Provedor {
     @OneToMany(mappedBy = "proveedor", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Compra> compras = new ArrayList<>();
 
-    public Provedor() {
-    }
-
-    public Provedor(Long id, String nombre, String rfc, String telefono, String email, String direccion, Boolean activo, List<Compra> compras) {
-        this.id = id;
-        this.nombre = nombre;
-        this.rfc = rfc;
-        this.telefono = telefono;
-        this.email = email;
-        this.direccion = direccion;
-        this.activo = activo;
-        this.compras = compras;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getRfc() {
-        return rfc;
-    }
-
-    public void setRfc(String rfc) {
-        this.rfc = rfc;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public Boolean getActivo() {
-        return activo;
-    }
-
-    public void setActivo(Boolean activo) {
-        this.activo = activo;
-    }
-
-    public List<Compra> getCompras() {
-        return compras;
-    }
-
-    public void setCompras(List<Compra> compras) {
-        this.compras = compras;
-    }
 }
